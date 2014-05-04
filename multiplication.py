7a8cd95e761e3e3a1520595e1bc211551ef85a67
Sam Stoelinga <sammiestoel@gmail.com>
2014-05-04 12:14:00 +0800

    Added support for different sizes of X and Y
    Previous version wasn't working as expected at all infact
    now I'm ensuring that multiplication is only done when
    we get to two digits.

--- multiplication.py
+++ multiplication.py
@@ -1,45 +1,66 @@
 import unittest
+from random import randint
 
 from math import ceil
+
+
+memory = [[0,0,0,0,0,0,0,0,0,0],
+          [0,1,2,3,4,5,6,7,8,9],
+          [0,2,4,6,8,10,12,14,16,18],
+          [0,3,6,9,12,15,18,21,24,27],
+          [0,4,8,12,16,20,24,28,32,36],
+          [0,5,10,15,20,25,30,35,40,45],
+          [0,6,12,18,24,30,36,42,48,54],
+          [0,7,14,21,28,35,42,49,56,63],
+          [0,8,16,24,32,40,48,56,64,72],
+          [0,9,18,27,36,45,54,63,72,81],
+          [0,10,20,30,40,50,60,70,80,90]]
+
+
 
 def mult(x, y):
     """ Recursive multiplication using
     x = 10^n/2 * a + b
     y = 10^n/2 * c + d
     x * y = 10^n * ac + 10^(n/2) (ad+bc) + bd
-    """
-    if x == 1:
-        return y
-    if y == 1:
-        return x
-    if x == 0 or y == 0:
-        return 0
 
+    Currently doesn't work for x=5432 y=12
+    """
     str_x, str_y = str(x), str(y)
     n = max(len(str_x), len(str_y))
-    if n <= 2:
-        return x * y
+    if n <= 1:
+        return memory[x][y]
 
-    n_2 = int(ceil(n/2.0))
+    n_2 = n / 2
 
-    if len(str_x) <= 2:
-        a , b = 0, x
+    if len(str_x) == 1:
+        a, b = 0, x
     else:
-        a, b = int(str_x[:n_2 - 1] or 0), int(str_x[n_2 - 1:] or 0)
-    if len(str_y) <= 2:
-        c , d = 0, y
+        str_x_2 = len(str_x) / 2
+        a, b = int(str_x[:str_x_2] or 0), int(str_x[str_x_2:] or 0)
+
+
+    if len(str_y) == 1:
+        c, d = 0, y
     else:
-        c, d = int(str_y[:n_2 - 1] or 0), int(str_y[n_2 - 1:] or 0)
+        str_y_2 = len(str_y) / 2
+        c, d = int(str_y[:str_y_2] or 0), int(str_y[str_y_2:] or 0)
+
+
+    print "a: %s, b: %s, c: %s, d: %s" % (a, b, c, d)
 
     ac = mult(a, c)
     ad_bc = mult(a, d) + mult(b, c)
     bd = mult(b, d)
 
-    print "a: %s, b: %s, c: %s, d: %s" % (a, b, c, d)
     print "ac: %s, ad + bc: %s, bd: %s" %(ac, ad_bc, bd)
+    print "n: %s, n/2: %s" % (n, n_2)
 
-    # Still can't see why 10^(n+1)
-    return (10**(n+1) * ac)  + (10**n_2 * ad_bc) + bd
+
+    # for supporting edge case where n is not a multiple of 2
+    n_2 = int(ceil(n / 2.0))
+    n = n if n % 2 == 0 else n + 1
+    return (10**(n) * ac)  + (10**n_2 * ad_bc) + bd
 
 
 class MultiplicationTestCase(unittest.TestCase):
@@ -49,7 +70,10 @@ class MultiplicationTestCase(unittest.TestCase):
         self.assertEqual(self.func(5, 5), 25)
 
     def test_different_size(self):
-        self.assertEqual(self.func(5, 155), 775)
+        self.assertEqual(self.func(2, 21), 42)
+
+    def test_different_size(self):
+        self.assertEqual(self.func(103, 3097), 318991)
 
     def test_two_digits1(self):
         self.assertEqual(self.func(50, 50), 2500)
@@ -61,10 +85,25 @@ class MultiplicationTestCase(unittest.TestCase):
         self.assertEqual(self.func(500, 500), 250000)
 
     def test_three_digits2(self):
-        self.assertEqual(self.func(256, 652), 166912)
+        self.assertEqual(self.func(223, 321), 71583)
+
+    def test_four_digits1(self):
+        self.assertEqual(self.func(1234, 4321), 5332114)
 
     def test_seven_digits(self):
-        self.assertEqual(self.func(5000000, 5000000), 25000000000000)
+        self.assertEqual(self.func(5000000, 5000000),
+                         25000000000000)
+
+    def test_random_cases(self):
+        for i in range(100):
+            x = randint(1,10000)
+            y = randint(1,10000)
+            expected = x * y
+            result = self.func(x, y)
+            self.assertEqual(self.func(x, y), expected,
+                            ("Failed with x: %s and y: %s. "
+                             "Expected: %s but got %s") % 
+                             (x, y, expected, result))
 
 
 class RecursiveMultiplicationTestCase(MultiplicationTestCase):
